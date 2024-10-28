98b70f0c26a4ec8979ac6a12c343fc10bcbf028c
mateolorenzo <mateolorenzo.dev@gmail.com>
2024-10-28 12:05:58 -0300

    Initial folder structure and swiper component for onboarding

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,49 @@
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        className="w-full flex justify-end items-end p-5"
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View>
+            <Text className="text-black text-2xl font-JakartaBold">
+              {item.title}
+            </Text>
+            <Text className="text-black text-base font-JakartaRegular">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
